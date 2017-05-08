f65045bf7095b5b2e1c370db645d19f76cf281e7
edwinchen85 <edwinchen85@gmail.com>
2017-05-09 00:47:42 +0800

    Create first Vue file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-
+    <h1>{{  title }}</h1>
+    <p>{{ greeting() }}</p>
   </div>
 </template>
 
@@ -8,6 +9,12 @@
 export default {
   data () {
     return {
+      title: 'Your first Vue file, wooo!'
+    }
+  },
+  methods: {
+    greeting: function() {
+      return 'heyyy cowboy!'
     }
   }
 }
